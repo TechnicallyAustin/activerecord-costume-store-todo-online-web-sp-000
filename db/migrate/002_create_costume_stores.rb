b9551b682324be8b0e854f24dc3e3304aad97a7a
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 21:02:22 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,6 +2,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change 
     create_table :costume_stores do |t|
       t.string :name
+      t.string :location
       t.integer :costume_inventory
       t.integer :employees
       t.boolean :in_business
