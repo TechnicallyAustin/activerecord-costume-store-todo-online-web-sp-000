0d2675328b84ca4d779c079c2d869605cdb17492
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 21:00:16 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :employees
       t.boolean :in_business
-      t.string :opening_time
-      t.string :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
